c0e510ff4bdcf2ed44f6388e82bb5081e08ed3c9
M584 <margaritka.vl.kov@mail.ru>
2018-04-20 19:50:03 +0700

    Created gui on the xaml

--- FotoFrameViewVM/MainWindow.xaml.cs
+++ FotoFrameViewVM/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FotoFrameViewVM
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
