864480f107ad2ea08d6d5e6c8dc6e46f258c6bdb
M584 <margaritka.vl.kov@mail.ru>
2018-04-20 21:01:56 +0700

    Added custom parameters to the model view

--- FotoFrameViewVM/PhotoFrameViewModel.cs
+++ FotoFrameViewVM/PhotoFrameViewModel.cs
@@ -1,10 +1,94 @@
 ﻿using System;
+using System.ComponentModel;
 using FotoFrameModel;
 
 namespace FotoFrameViewVM
 {
-    public class PhotoFrameViewModel
+    public class PhotoFrameViewModel : INotifyPropertyChanged,
+        IDataErrorInfo
     {
+        private PhotoFrameTemplate _photoFrame;
 
+        public PhotoFrameViewModel()
+        {
+            const double minHeight = 1.0f;
+            const double minForLengthAndWidth = 10.0f;
+            const double max = 100.0f;
+            const double maxInterval = 6.0f;
+            const double value = 5.0f;
+
+            var outerWidth = new BorderConditions(minForLengthAndWidth,
+                max, max);
+            var outerHeight = new BorderConditions(minHeight,
+                value, minForLengthAndWidth);
+            var outerLength = new BorderConditions(minForLengthAndWidth,
+                max, max);
+            var innerHeight = new BorderConditions(minHeight,
+                value, minForLengthAndWidth);
+            var interval = new BorderConditions(minHeight,
+                value, maxInterval);
+
+            _photoFrame = new PhotoFrameTemplate(outerWidth,
+                outerHeight,
+                outerLength,
+                innerHeight,
+                interval);
+        }
+
+        public double OuterWidth
+        {
+            get
+            {
+                return _photoFrame.OuterWidth;
+            }
+            set
+            {
+                _photoFrame.OuterWidth = value;
+            }
+        }
+
+        public double OuterHeight {
+            get
+            {
+                return _photoFrame.OuterHeight;
+            }
+            set
+            {
+                _photoFrame.OuterHeight = value;
+            }
+        }
+
+        public double OuterLength {
+            get
+            {
+                return _photoFrame.OuterLength;
+            }
+            set
+            {
+                _photoFrame.OuterLength = value;
+            }
+        }
+
+        public double InnerHeight {
+            get
+            {
+                return _photoFrame.InnerHeight;
+            }
+            set
+            {
+                _photoFrame.InnerHeight = value;
+            }
+        }
+
+        public double Interval {
+            get
+            {
+                return _photoFrame.Interval;
+            }
+            set
+            {
+                _photoFrame.Interval = value;
+            }
+        }
     }
 }
