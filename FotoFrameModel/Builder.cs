1a84bdd880fd7afee51bd5394c46a9c183b1dd91
M584 <margaritka.vl.kov@mail.ru>
2018-04-21 18:06:51 +0700

    Added xml-comments for members of builder photo frames

--- FotoFrameModel/Builder.cs
+++ FotoFrameModel/Builder.cs
@@ -2,14 +2,33 @@
 
 namespace FotoFrameModel
 {
+    /// <summary>
+    /// Предоставляет возможность строить модель
+    ///     по параметрам фоторамки в САПР
+    /// </summary>
     public interface IBuilder
     {
+        /// <summary>
+        /// Построить модель фоторамки в САПР
+        /// </summary>
+        /// <param name="photoFrame">Шаблон фоторамки</param>
+        /// <param name="checker">Проверяющий параметры фоторамки</param>
         void Build(IPhotoFrame photoFrame, IChecker checker);
     }
 
-
+    /// <summary>
+    /// Мастер по созданию фоторамок в САПР Компас 3D
+    /// </summary>
     public class BuilderPhotoFrame : IBuilder
     {
+        /// <summary>
+        /// Построить модель фоторамки в САПР Компас 3D
+        /// </summary>
+        /// <param name="photoFrame">Шаблон фоторамки</param>
+        /// <param name="checker">Проверяющий параметры фоторамки</param>
+        /// <exception cref="InvalidOperationException">
+        ///     Вызывается тогда, когда параметры фоторамки
+        ///     имеют недопустимые значения.</exception>
         public void Build(IPhotoFrame photoFrame, IChecker checker)
         {
             if(checker.IsValid == false)
